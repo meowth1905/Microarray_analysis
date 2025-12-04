e3dd9390088c91d55620381e91c793cf0c0431e5
cookie1905 <neethu51193@gmail.com>
2025-12-04 18:51:48 +0100

    added code

--- microarray_analysis_4.R
+++ microarray_analysis_4.R
@@ -1,26 +1,33 @@
-setwd("C:\\Neethu\\MicroArrayAnalysis")
+setwd("C:\\Neethu\\Data_Analysis_With_R")
 
 list.files()
-pheno.data <- read.csv("assig6_data\\arrays.txt", sep = "\t", header = TRUE)
+pheno.data <- read.csv("assig6_data\\arrays.txt", sep = "\t", header = TRUE,
+                       row.names = 2)
 dim(pheno.data)
-
-# HT: Hypothalamus
-# GF: Gonadal fat
 
 array.data <- read.csv("assig6_data\\arraydata.txt", sep = "\t", header = TRUE)
 dim(array.data)
 
-array.num <- array.data[, 2:17]
-boxplot(array.num, las = 2, cex.axis = 0.7)
+# Make a Boxplot
+boxplot(array.data[, rownames(pheno.data)], las = 2, cex.axis = 0.7)
+# intensity data is not normal (as expected)
 
-array.log <- apply(array.num, 2, log2)
+# Log2 transformation of array data
+array.log <- log2(array.data[, rownames(pheno.data)])
+
+# Make a Boxplot
 boxplot(array.log, las = 2, cex.axis = 0.7)
 
+# Normalization
 library(preprocessCore)
-array.norm <- normalize.quantiles(array.log)
+
+class(array.log)
+
+array.norm <- normalize.quantiles(as.matrix(array.log))
 colnames(array.norm) <- colnames(array.log)
 rownames(array.norm) <- rownames(array.log)
 
+# Make a Boxplot
 boxplot(array.norm, las = 2, cex.axis = 0.7)
 
 old.par <- par()
@@ -116,3 +123,168 @@ results$sigAdjusted <- results$pAdjusted < 0.05
 table(results$sig)
 table(results$sigAdjusted)
 
+
+# Fitting a linear model
+# 2 entries (HT and GF) for each Individual (here mouse)
+# example: pheno.data[pheno.data$Individual=="513",]
+# Linear model we assume observations are independent.
+# Not the case here
+# So we split data
+
+
+# linear model for HT Tissue
+#
+pheno.data[,"Tissue"] == "HT"
+# or
+pheno.data$Tissue == "HT"
+
+which(pheno.data$Tissue == "HT")
+HT.samples <- rownames(pheno.data)[which(pheno.data$Tissue == "HT")]
+
+# expression values of 1st probe for HT samples
+array.norm[1, HT.samples]
+
+# strains of HT samples
+# there are 3 different strains: "B6N"  "BFMI" "F1" 
+pheno.data[HT.samples, "Strain"]    
+
+row.number <- 500
+expr <- array.norm[row.number, HT.samples]
+strain <- factor(pheno.data[HT.samples, "Strain"])
+lm(expr ~ strain)
+summary(lm(expr ~ strain))
+
+# Or use anova to get p value
+anova(lm(expr ~ strain))
+anova(lm(expr ~ strain))["strain", "Pr(>F)"]
+
+p.vals <- c()
+for (i in 1: nrow(array.norm)){
+  expr <- array.norm[i, HT.samples]
+  strain <- factor(pheno.data[HT.samples, "Strain"])
+  p.val <- anova(lm(expr ~ strain))["strain", "Pr(>F)"]
+  p.vals <- c(p.vals, p.val)
+}
+
+# Adjusting p values for multiple testing
+p.adjusted <- p.adjust(p.vals, method = "fdr")
+
+# How many below 0.05
+length(which(p.adjusted < 0.05))
+
+# so in 1198 probes one of the strain is different from the other two
+
+# 
+# To find which probes are different between
+# BFMI and B6n
+# F1 and BFMI
+# PostHoc tests for figuring which of the strains are really different
+
+which(p.adjusted < 0.05)
+array.norm[88, HT.samples]
+
+strain <- pheno.data[HT.samples, "Strain"]
+array.norm[88, HT.samples][which(strain == "F1")]
+
+n.tests <- length(which(p.adjusted < 0.05))
+bfmi.b6n <- 0; bfmi.f1 <- 0
+b6n.probes <- c(); f1.probes <- 0
+
+for (i in which(p.adjusted < 0.05)){
+  expr <- array.norm[i, HT.samples]
+  strain <- factor(pheno.data[HT.samples, "Strain"])
+  
+  bfmi <- expr[which(strain == "BFMI")]
+  f1 <- expr[which(strain == "F1")]
+  b6n <- expr[which(strain == "B6N")]
+  
+  t1 <- t.test(bfmi, b6n)
+  t2<- t.test(bfmi, f1)
+  
+  if (p.adjust(t1$p.value, "fdr", n = n.tests) < 0.05){
+    bfmi.b6n <- bfmi.b6n + 1
+    b6n.probes <- c(b6n.probes, i)
+  }
+  
+  if (p.adjust(t2$p.value, "fdr", n = n.tests) < 0.05){
+    bfmi.f1 <- bfmi.f1 + 1
+    f1.probes <- c(f1.probes, i)
+  }
+
+}
+
+bfmi.b6n
+# 96 probes different between bfmi and b6n 
+
+bfmi.f1
+# 40 probes different between bfmi and b6n
+
+# Instead of copying above code and replacing HT with GF
+
+linear.model <- function(normalized.array.data, samples){
+  p.vals <- c()
+  for (i in 1: nrow(normalized.array.data)){
+    expr <- normalized.array.data[i, samples]
+    strain <- factor(pheno.data[samples, "Strain"])
+    p.val <- anova(lm(expr ~ strain))["strain", "Pr(>F)"]
+    p.vals <- c(p.vals, p.val)
+  }
+  return(p.vals)
+}
+
+posthoc <- function(p.adjusted, samples, pheno.data, normalized.array.data){
+  
+  bfmi.b6n <- 0; bfmi.f1 <- 0; b6n.f1 <- 0
+  b6n.probes <- c(); f1.probes <- 0; b6.f1.probes <- c()
+  
+  for (i in which(p.adjusted < 0.05)){
+    expr <- normalized.array.data[i, samples]
+    strain <- factor(pheno.data[samples, "Strain"])
+    
+    bfmi <- expr[which(strain == "BFMI")]
+    f1 <- expr[which(strain == "F1")]
+    b6n <- expr[which(strain == "B6N")]
+    
+    t1 <- t.test(bfmi, b6n)
+    t2 <- t.test(bfmi, f1)
+    t3 <- t.test(b6n, f1)
+    
+    n.tests <- length(which(p.adjusted < 0.05))
+    
+    if (p.adjust(t1$p.value, "fdr", n = n.tests) < 0.05){
+      bfmi.b6n <- bfmi.b6n + 1
+      b6n.probes <- c(b6n.probes, i)
+    }
+    
+    if (p.adjust(t2$p.value, "fdr", n = n.tests) < 0.05){
+      bfmi.f1 <- bfmi.f1 + 1
+      f1.probes <- c(f1.probes, i)
+    }
+    
+    if (p.adjust(t3$p.value, "fdr", n = n.tests) < 0.05){
+      b6n.f1 <- b6n.f1 + 1
+      b6.f1.probes <- c(b6.f1.probes, i)
+    }
+
+  }
+  return(list("bfmi vs b6n"=bfmi.b6n, "bfmi vs f1"=bfmi.f1, "b6n vs f1"=b6n.f1,
+              "probe index bfmi vs b6n"=b6n.probes, "probe index bfmi vs f1"=f1.probes,
+              "probe index b6n vs f1"=b6.f1.probes))
+}
+
+
+p.adj.HT <- p.adjust(linear.model(array.norm, HT.samples), method = "fdr")
+length(which(p.adj.HT < 0.05))
+
+result.HT <- posthoc(p.adj.HT, HT.samples, pheno.data, array.norm)
+result.HT
+
+
+# GF samples
+GF.samples <- rownames(pheno.data)[which(pheno.data$Tissue == "GF")]
+
+p.adj.GF <- p.adjust(linear.model(array.norm, GF.samples), method = "fdr")
+length(which(p.adj.GF < 0.05))
+
+result.GF <- posthoc(p.adj.GF, GF.samples, pheno.data, array.norm)
+result.GF
